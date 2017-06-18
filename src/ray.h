90dcfdd0fd9a903397307d74a278d9d0478a0e67
Takahiro Yaota <z@kuro.red>
2017-06-18 22:34:42 +0900

    Add ray.h

---
+++ src/ray.h
@@ -0,0 +1,10 @@
+#ifndef Z_RAY_ee19b0554a7a4ab28d5b8f8433726622
+#define Z_RAY_ee19b0554a7a4ab28d5b8f8433726622
+
+#include "vec.h"
+
+typedef struct ray {
+    vec3_t origin, dir;
+} ray_t;
+
+#endif
