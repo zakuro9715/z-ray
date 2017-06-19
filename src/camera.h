80f638acfffc75969c5808c43d95d738a562a002
Takahiro Yaota <z@kuro.red>
2017-06-19 14:12:17 +0900

    Add camera.h

---
+++ src/camera.h
@@ -0,0 +1,10 @@
+#ifndef Z_RAY_06b304be6ab04309b06265e2910539e7
+#define Z_RAY_06b304be6ab04309b06265e2910539e7
+
+#include "vec.h"
+
+typedef struct camera {
+    vec3_t origin, dir, up;
+} camera_t;
+
+#endif
