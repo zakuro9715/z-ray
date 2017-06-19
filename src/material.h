ad7c5129e7c98b94c788535732a378eb36ca681e
Takahiro Yaota <z@kuro.red>
2017-06-19 19:58:47 +0900

    Add material.h

---
+++ src/material.h
@@ -0,0 +1,13 @@
+#ifndef Z_RAY_2778a02c5e964641b6ecd24eed28a17b
+#define Z_RAY_2778a02c5e964641b6ecd24eed28a17b
+
+#include "color.h"
+
+#define REFLECTION_TYPE_LAMBERT (1)
+
+typedef struct material {
+    color_t color, emission;
+    char reflection_type;
+} material_t;
+
+#endif
