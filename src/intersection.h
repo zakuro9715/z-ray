af09e48a1201b9e7f4d7e9832f6074ec2548f068
Takahiro Yaota <z@kuro.red>
2017-06-20 15:42:55 +0900

    Add intersection.h

---
+++ src/intersection.h
@@ -0,0 +1,13 @@
+#ifndef Z_RAY_3eb8686e1bfb4db9a095f9c75138db74
+#define Z_RAY_3eb8686e1bfb4db9a095f9c75138db74
+
+typedef struct ray_hit {
+    double distance;
+    vec3_t point, normal;
+} ray_hit_t;
+
+typedef struct intersection {
+    ray_hit_t hit;
+} intersection_t;
+
+#endif
