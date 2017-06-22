f32bd4eca2ae0452894225870992cf6d32a091f0
Takahiro Yaota <z@kuro.red>
2017-06-22 10:03:27 +0900

    Replace rand algorithm with Xorshift128

--- src/random.c
+++ src/random.c
@@ -1,14 +1,26 @@
+
 #include "random.h"
 #include <stdlib.h>
+#include <stdint.h>
 
-// TODO: Use better algorithm(XorShift, etc...)
-
-double random_next()
+uint32_t xor128()
 {
-    return (double)rand() / RAND_MAX;
+    static uint32_t x = 123456789;
+    static uint32_t y = 362436069;
+    static uint32_t z = 521288629;
+    static uint32_t w =  88675123;
+    uint32_t t;
+
+    t = x^ (x << 11);
+    x = y;
+    y = z;
+    z = w;
+    w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+
+    return w;
 }
 
-unsigned int random_next_uint()
-{
-    return rand();
+double random_next()
+{    
+    return (double)xor128() / (double)(4294967295);
 }
--- src/random.h
+++ src/random.h
@@ -2,6 +2,5 @@
 #define Z_RAY_2bfda48e0f8d452d98cd27e3b629f8b9
 
 double random_next();
-unsigned int random_next_uint();
 
 #endif
